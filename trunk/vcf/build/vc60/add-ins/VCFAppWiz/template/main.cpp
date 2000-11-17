ac5d2d2a349abef5a2f4467604409d21bc91267c
esomogyi <esomogyi@cccfda39-7811-0410-ae5f-bc4b32ab4073>
2000-11-17 08:32:06 +0000

    *** empty log message ***

---
+++ trunk/vcf/build/vc60/add-ins/VCFAppWiz/template/main.cpp
@@ -0,0 +1,36 @@
+//$$root$$.cpp
+
+
+#include "Application.h"
+#include "Window.h"
+#include "Rect.h"
+
+using namespace VCF;
+
+
+class $$Root$$Application : public Application {
+public:
+
+	virtual bool initRunningApplication(){
+		bool result = Application::initRunningApplication();
+		
+		Window* helloWorldWindow = new Window();
+		setMainWindow(helloWorldWindow);
+		helloWorldWindow->setBounds( &Rect( 100.0, 100.0, 500.0, 500.0 ) );
+		helloWorldWindow->setCaption( "Hello World !!!!" );
+
+		return result;
+	}
+
+};
+
+
+int main(int argc, char *argv[])
+{
+	MyApplication app;
+
+	Application::appMain( argc, argv );
+	
+	return 0;
+}
+
