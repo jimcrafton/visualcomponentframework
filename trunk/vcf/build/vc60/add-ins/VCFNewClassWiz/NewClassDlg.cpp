96c0974695dfb1691609e5ab6c0b12d431974938
ddiego <ddiego@cccfda39-7811-0410-ae5f-bc4b32ab4073>
2000-11-20 21:48:01 +0000

    *** empty log message ***

---
+++ trunk/vcf/build/vc60/add-ins/VCFNewClassWiz/NewClassDlg.cpp
@@ -0,0 +1,438 @@
+// NewClassDlg.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "vcfnewclasswiz.h"
+#include "NewClassDlg.h"
+#include "Rpcdce.h" //make sure to link with Rpcrt4.lib
+#include "PropertyInfoDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// NewClassDlg dialog
+
+
+NewClassDlg::NewClassDlg(CWnd* pParent /*=NULL*/)
+	: CDialog(NewClassDlg::IDD, pParent)
+{
+	//{{AFX_DATA_INIT(NewClassDlg)
+	m_className = _T("");
+	m_CPPName = _T("");
+	m_derivesFrom = _T("VCF::Object");
+	m_headerName = _T("");
+	m_supportsRTTI = TRUE;
+	m_abstractClass = FALSE;
+	//}}AFX_DATA_INIT
+
+	m_headerDir = "";
+	m_cppDir = "";
+}
+
+
+void NewClassDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	//{{AFX_DATA_MAP(NewClassDlg)
+	DDX_Control(pDX, IDC_CPP, m_cppEdit);
+	DDX_Control(pDX, IDC_HEADER, m_headerEdit);
+	DDX_Control(pDX, IDC_PROP_LIST, m_propList);
+	DDX_Control(pDX, IDC_DERIVES_FROM, m_derivesList);
+	DDX_Text(pDX, IDC_CLASS_NAME, m_className);
+	DDX_Text(pDX, IDC_CPP, m_CPPName);
+	DDX_CBString(pDX, IDC_DERIVES_FROM, m_derivesFrom);
+	DDX_Text(pDX, IDC_HEADER, m_headerName);
+	DDX_Check(pDX, IDC_SUPPORTS_VCF_RTTI, m_supportsRTTI);
+	DDX_Check(pDX, IDC_ABSTRACT_CLASS, m_abstractClass);
+	//}}AFX_DATA_MAP
+
+	if ( pDX->m_bSaveAndValidate ){
+		m_classDecl = "";
+
+		UUID id;
+		CString classid;
+		if ( RPC_S_OK == ::UuidCreate( &id ) ){
+			unsigned char *tmpid = NULL;
+			
+			RPC_STATUS rpcresult = UuidToString(  &id, &tmpid );
+			
+			if ( RPC_S_OUT_OF_MEMORY != rpcresult ) {
+				classid = tmpid;
+				
+				RpcStringFree( &tmpid );
+			}
+			else {
+				classid = "\"unknown\"";
+			}
+		}
+		
+
+		CString defName = m_className;
+		defName.MakeUpper();
+		defName = "_" + defName + "_H__";
+		m_classDecl = "//" + m_className + ".h\n\n";
+		m_classDecl += "#ifndef " + defName + "\n";
+		m_classDecl += "#define " + defName + "\n\n\n";
+
+		CString classIDName = m_className;
+		classIDName.MakeUpper();
+		classIDName += "_CLASSID";
+		m_classDecl += "#include \"VCF.h\"\n";
+		m_classDecl += "#include \"ClassInfo.h\"\n\n\nusing namespace VCF;\n\n\n";
+		if ( this->m_supportsRTTI ){
+			m_classDecl += "#define " + classIDName + "\t\t\"" + classid + "\"\n\n\n";
+		}
+		m_classDecl += "class " + m_className + " : public " + m_derivesFrom + " { \n";
+		m_classDecl += "public:\n";
+		if ( this->m_supportsRTTI ){
+			if ( this->m_abstractClass ){
+				m_classDecl += "\tBEGIN_ABSTRACT_CLASSINFO(" + m_className + ", \"" + m_className + "\", \"" + m_derivesFrom + "\", " + classIDName + ")\n";	
+			}
+			else {
+				m_classDecl += "\tBEGIN_CLASSINFO(" + m_className + ", \"" + m_className + "\", \"" + m_derivesFrom + "\", " + classIDName + ")\n";
+			}
+			this->fillInPropInfo();	
+			m_classDecl += "\tEND_CLASSINFO(" + m_className + ")\n\n";
+			
+		}
+		m_classDecl += "\t" + m_className + "();\n\n";
+		m_classDecl += "\tvirtual ~" + m_className + "();\n\n";
+
+		fillInPropMethods();
+
+		m_classDecl += "protected:\n\nprivate:\n";
+
+		fillInPropMemberVars();
+
+		m_classDecl += "};\n\n\n";
+
+		m_classDecl += "#endif //" + defName + "\n\n\n";
+
+		this->m_classImpl = "//" + m_className + ".h\n\n";
+
+		m_classImpl += "#include \"" + m_className + ".h\"\n\n";
+
+		m_classImpl += "using namespace VCF;\n\n\n";
+
+		m_classImpl += m_className + "::" + m_className + "()\n{\n\n}\n\n";
+
+		m_classImpl += m_className + "::~" + m_className + "()\n{\n\n}\n\n";
+		
+		fillInPropMethodsImpl();
+	}
+}
+
+
+BEGIN_MESSAGE_MAP(NewClassDlg, CDialog)
+	//{{AFX_MSG_MAP(NewClassDlg)
+	ON_BN_CLICKED(IDC_CHANGE_CPP_BTN, OnChangeCppBtn)
+	ON_BN_CLICKED(IDC_CHANGE_HDR_BTN, OnChangeHdrBtn)
+	ON_EN_CHANGE(IDC_CLASS_NAME, OnChangeClassName)
+	ON_BN_CLICKED(IDC_SUPPORTS_VCF_RTTI, OnSupportsVcfRtti)
+	ON_NOTIFY(HDN_ITEMDBLCLICK, IDC_PROP_LIST, OnItemdblclickPropList)
+	ON_NOTIFY(NM_RCLICK, IDC_PROP_LIST, OnRclickPropList)
+	ON_COMMAND(ID_PROPSPOPUP_EDIT, OnPropspopupEdit)
+	ON_COMMAND(ID_PROPSPOPUP_REMOVE, OnPropspopupRemove)
+	ON_COMMAND(ID_PROPSPOPUP_ADD, OnPropspopupAdd)
+	ON_WM_CREATE()
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// NewClassDlg message handlers
+
+void NewClassDlg::OnChangeCppBtn() 
+{
+	CFileDialog saveAs( FALSE, NULL, this->m_CPPName, 
+		                OFN_HIDEREADONLY | OFN_OVERWRITEPROMPT, 
+						"C++ Files|*.cpp||" );
+
+	if ( IDOK == saveAs.DoModal() ){
+		m_CPPName = saveAs.GetPathName ();
+		int pos = m_CPPName.ReverseFind( '\\' );
+		if ( -1 != pos ){
+			m_cppDir = m_CPPName.Left( pos );
+			if ( '\\' != m_cppDir.GetAt(m_cppDir.GetLength()-1) ){
+				m_cppDir += "\\";
+			}
+		}
+		this->m_cppEdit.EnableWindow();
+		this->UpdateData(FALSE);
+	}
+}
+
+void NewClassDlg::OnChangeHdrBtn() 
+{
+	CFileDialog saveAs( FALSE, NULL, this->m_headerName, 
+		                OFN_HIDEREADONLY | OFN_OVERWRITEPROMPT, 
+						"C++ Header Files|*.h||" );
+
+	if ( IDOK == saveAs.DoModal() ){
+		m_headerName = saveAs.GetPathName ();
+		int pos = m_headerName.ReverseFind( '\\' );
+		if ( -1 != pos ){
+			m_headerDir = m_headerName.Left( pos );
+			if ( '\\' != m_headerDir.GetAt(m_headerDir.GetLength()-1) ){
+				m_headerDir += "\\";
+			}
+		}
+		this->m_headerEdit.EnableWindow();
+		this->UpdateData(FALSE);
+	}
+}
+
+void NewClassDlg::OnChangeClassName() 
+{
+	this->UpdateData();
+
+	this->m_CPPName =  m_cppDir + this->m_className + ".cpp";
+	this->m_headerName = m_headerDir + this->m_className + ".h";
+
+	this->UpdateData(FALSE);
+}
+
+void NewClassDlg::OnSupportsVcfRtti() 
+{
+	this->UpdateData();
+	CWnd* wnd = this->GetDlgItem(IDC_ABSTRACT_CLASS);	
+	wnd->EnableWindow( this->m_supportsRTTI );
+	wnd = this->GetDlgItem(IDC_STATIC_PROPS);	
+	wnd->EnableWindow( this->m_supportsRTTI );
+	wnd = this->GetDlgItem(IDC_PROP_LIST);	
+	wnd->EnableWindow( this->m_supportsRTTI );
+}
+
+void NewClassDlg::OnItemdblclickPropList(NMHDR* pNMHDR, LRESULT* pResult) 
+{
+	HD_NOTIFY *phdn = (HD_NOTIFY *) pNMHDR;
+	
+	
+	*pResult = 0;
+}
+
+void NewClassDlg::OnRclickPropList(NMHDR* pNMHDR, LRESULT* pResult) 
+{	
+	CMenu popup;
+	popup.LoadMenu( IDR_PROPSMNU );
+	POINT pt = {0};
+	::GetCursorPos( &pt );
+	popup.GetSubMenu(0)->TrackPopupMenu( TPM_LEFTALIGN, pt.x, pt.y, this );
+	*pResult = 0;
+}
+
+void NewClassDlg::OnPropspopupEdit() 
+{
+	// TODO: Add your command handler code here
+	
+}
+
+void NewClassDlg::OnPropspopupRemove() 
+{
+	// TODO: Add your command handler code here
+	
+}
+
+void NewClassDlg::OnPropspopupAdd() 
+{
+	PropertyInfoDlg dlg;
+	if ( IDOK == dlg.DoModal() ){
+		CString name = dlg.m_propName;
+		CString type = dlg.m_propType;
+
+		PropInfo* info = new PropInfo();
+		info->Name = name;
+		info->Type = type;
+		info->readOnly = (bool)dlg.m_readOnlyProp;
+		
+		LVITEM item = {0};
+		item.mask = LVIF_TEXT | LVIF_PARAM;
+		item.cchTextMax = name.GetLength();
+		item.pszText = name.GetBuffer(0);
+		item.lParam = (LPARAM)info;
+		int i = m_propList.InsertItem( &item );
+		
+		m_propList.SetItemText( i, 1, type );
+		CString readOnly = dlg.m_readOnlyProp ? "Yes" : "No";
+		
+		m_propList.SetItemText( i, 2, readOnly );
+		
+	}	
+}
+
+int NewClassDlg::OnCreate(LPCREATESTRUCT lpCreateStruct) 
+{
+	if (CDialog::OnCreate(lpCreateStruct) == -1)
+		return -1;
+	
+	// TODO: Add your specialized creation code here
+	
+	return 0;
+}
+
+BOOL NewClassDlg::OnInitDialog() 
+{
+	CDialog::OnInitDialog();
+	
+	this->m_propList.InsertColumn( 0, "Name" );
+	this->m_propList.SetColumnWidth(0, 100 );
+	this->m_propList.InsertColumn( 1, "Type" );
+	this->m_propList.SetColumnWidth(1, 80 );
+
+	this->m_propList.InsertColumn( 2, "Read Only" );
+	this->m_propList.SetColumnWidth(2, 50 );
+	
+	return TRUE;  // return TRUE unless you set the focus to a control
+	              // EXCEPTION: OCX Property Pages should return FALSE
+}
+
+
+void NewClassDlg::fillInPropInfo()
+{
+	int count = m_propList.GetItemCount();
+	for (int i = 0;i<count;i++){
+		PropInfo* info = (PropInfo*)m_propList.GetItemData( i );
+		if ( NULL != info ){
+			CString name = m_propList.GetItemText(i , 0 );
+			CString type = m_propList.GetItemText(i , 1 );
+			char c = name.GetAt(0);
+			c = toupper(c);
+			CString s_funcName = this->m_className + "::set";
+			s_funcName += c;
+			CString tmp = name;
+			tmp.Delete(0);
+			s_funcName += tmp;
+
+			CString g_funcName = this->m_className + "::get";
+			g_funcName += c;
+			g_funcName += tmp;
+
+
+			CString typeName;
+			if ( type == "int" ){
+				typeName = "PROP_INT";	
+			}
+			else if ( type == "long" ){
+				typeName = "PROP_LONG";	
+			}
+			else if ( type == "short" ){
+				typeName = "PROP_SHORT";	
+			}
+			else if ( type == "unsigned long" ){
+				typeName = "PROP_ULONG";	
+			}
+			else if ( type == "float" ){
+				typeName = "PROP_FLOAT";	
+			}
+			else if ( type == "char" ){
+				typeName = "PROP_CHAR";	
+			}
+			else if ( type == "double" ){
+				typeName = "PROP_DOUBLE";	
+			}
+			else if ( type == "bool" ){
+				typeName = "PROP_BOOL";	
+			}
+			else if ( type == "String" ){
+				typeName = "PROP_STRING";	
+			}
+
+			CString prop;
+			if ( info->readOnly ){
+				prop = "\tREADONLY_PROPERTY(" + type + ", " + "\"" + name + "\", " + g_funcName;
+			}
+			else {
+				prop = "\tPROPERTY(" + type + ", " + "\"" + name + "\", " + g_funcName;
+			}
+			
+			if ( !info->readOnly ){
+				prop += ", " + s_funcName;
+			}
+			prop += ", " + typeName + ");\n";
+			m_classDecl += prop;
+		}	
+	}
+}
+
+void NewClassDlg::fillInPropMethods()
+{
+	if ( this->m_supportsRTTI ){
+		int count = m_propList.GetItemCount();
+		for (int i = 0;i<count;i++){
+			PropInfo* info = (PropInfo*)m_propList.GetItemData( i );
+			if ( NULL != info ){
+				CString name = m_propList.GetItemText(i , 0 );
+				CString type = m_propList.GetItemText(i , 1 );
+				char c = name.GetAt(0);
+				c = toupper(c);
+				CString s_funcName = "set";
+				s_funcName += c;
+				CString tmp = name;
+				tmp.Delete(0);
+				s_funcName += tmp;
+				
+				CString g_funcName = "get";
+				g_funcName += c;
+				g_funcName += tmp;				
+				
+				CString propGetMethod = "\t" + type + " " + g_funcName + "();\n\n";
+				m_classDecl += 	propGetMethod;
+				if ( !info->readOnly ){
+					CString propSetMethod = "\tvoid " + s_funcName + "( const " + type + "& " + name + " );\n\n";
+					m_classDecl += 	propSetMethod;
+				}
+			}	
+		}
+	}
+}
+
+void NewClassDlg::fillInPropMethodsImpl()
+{
+	if ( this->m_supportsRTTI ){
+		int count = m_propList.GetItemCount();
+		for (int i = 0;i<count;i++){
+			PropInfo* info = (PropInfo*)m_propList.GetItemData( i );
+			if ( NULL != info ){
+				CString name = m_propList.GetItemText(i , 0 );
+				CString type = m_propList.GetItemText(i , 1 );
+				char c = name.GetAt(0);
+				c = toupper(c);
+				CString s_funcName = m_className + "::set";
+				s_funcName += c;
+				CString tmp = name;
+				tmp.Delete(0);
+				s_funcName += tmp;
+				
+				CString g_funcName = m_className + "::get";
+				g_funcName += c;
+				g_funcName += tmp;				
+				
+				CString propGetMethod = type + " " + g_funcName + "()\n{\n\treturn m_" + name + ";\n}\n\n";
+				m_classImpl += 	propGetMethod;
+				if ( !info->readOnly ){
+					CString propSetMethod = "void " + s_funcName + "( const " + type + "& " + name + " )\n{\n\tm_" + name + " = " + name + ";\n}\n\n";
+					m_classImpl += 	propSetMethod;
+				}
+			}	
+		}
+	}
+}
+
+void NewClassDlg::fillInPropMemberVars()
+{
+	if ( this->m_supportsRTTI ){
+		int count = m_propList.GetItemCount();
+		for (int i = 0;i<count;i++){
+			PropInfo* info = (PropInfo*)m_propList.GetItemData( i );
+			if ( NULL != info ){
+				CString name = m_propList.GetItemText(i , 0 );
+				CString type = m_propList.GetItemText(i , 1 );
+
+				m_classDecl += "\t" + type + " m_" + name + ";\n";
+			}	
+		}
+	}
+}
---
+++ trunk/vcf/build/vc60/add-ins/VCFNewClassWiz/resource.h
@@ -0,0 +1,43 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by VCFNewClassWiz.rc
+//
+#define IDR_TOOLBAR_MEDIUM              128
+#define IDR_TOOLBAR_LARGE               129
+#define IDD_UNDOCUMENTEDVC              1000
+#define IDC_TREE_CLASSES                1000
+#define IDC_LIST_CLASSES                1001
+#define IDC_MODE_REPORT                 1002
+#define IDC_MODE_GRAPH                  1003
+#define IDD_NEWCLASSDLG                 1003
+#define IDC_PROP_NAME                   1003
+#define IDD_PROPERTY                    1004
+#define IDC_DERIVES_FROM                1004
+#define IDC_CHANGE_HDR_BTN              1005
+#define IDR_PROPSMNU                    1006
+#define IDC_READ_ONLY_PROP              1006
+#define IDR_VCFNewClassWiz              1007
+#define IDR_COMMANDS                    1008
+#define IDC_PROP_TYPE                   1008
+#define IDC_CHANGE_CPP_BTN              1008
+#define IDC_CLASS_NAME                  1009
+#define IDC_HEADER                      1010
+#define IDC_CPP                         1011
+#define IDC_SUPPORTS_VCF_RTTI           1012
+#define IDC_PROP_LIST                   1013
+#define IDC_ABSTRACT_CLASS              1014
+#define IDC_STATIC_PROPS                1015
+#define ID_PROPSPOPUP_ADD               32771
+#define ID_PROPSPOPUP_EDIT              32772
+#define ID_PROPSPOPUP_REMOVE            32773
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        1008
+#define _APS_NEXT_COMMAND_VALUE         32774
+#define _APS_NEXT_CONTROL_VALUE         1016
+#define _APS_NEXT_SYMED_VALUE           1009
+#endif
+#endif
