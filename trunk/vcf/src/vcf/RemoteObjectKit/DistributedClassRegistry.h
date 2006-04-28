ab5289a9bf5b6d0f55f47713dd35b38ce997fcfa
kdmix <kdmix@cccfda39-7811-0410-ae5f-bc4b32ab4073>
2006-04-28 17:36:47 +0000

    CVS Log info removed. Id keyword added.

--- trunk/vcf/src/vcf/RemoteObjectKit/DistributedClassRegistry.h
+++ trunk/vcf/src/vcf/RemoteObjectKit/DistributedClassRegistry.h
@@ -72,50 +72,8 @@ private:
 }; //end of namespace VCFRemote
 
 
-/**
-*CVS Log info
-*$Log$
-*Revision 1.2  2004/08/07 02:49:20  ddiego
-*merged in the devmain-0-6-5 branch to stable
-*
-*Revision 1.1.2.2  2004/04/29 04:12:58  marcelloptr
-*reformatting of source files: macros and csvlog and copyright sections
-*
-*Revision 1.1.2.1  2004/04/28 03:45:50  ddiego
-*migration towards new directory structure
-*
-*Revision 1.7.8.1  2004/04/26 21:58:46  marcelloptr
-*changes for dir reorganization: _VCF_MACRO_H__
-*
-*Revision 1.7  2003/05/17 20:37:16  ddiego
-*this is the checkin for the 0.6.1 release - represents the merge over from
-*the devmain-0-6-0 branch plus a few minor bug fixes
-*
-*Revision 1.6.16.1  2003/03/12 03:11:48  ddiego
-*switched all member variable that used the "m_"<name> prefix to
-* <name>"_" suffix nameing standard.
-*Also changed all vcf builder files to accomadate this.
-*Changes were made to the Stream classes to NOT multiple inheritance and to
-*be a little more correct. Changes include breaking the FileStream into two
-*distinct classes, one for input and one for output.
-*
-*Revision 1.6  2002/05/09 03:10:43  ddiego
-*merged over code from development branch devmain-0-5-1a into the main CVS trunk
-*
-*Revision 1.5.4.1  2002/04/03 19:37:17  zzack
-*include style changed
-*
-*Revision 1.5  2002/02/27 04:16:11  ddiego
-*add sub items to TreeItem class for teh tree list control
-*added fixes to the RemoteObject stuff
-*
-*Revision 1.4  2002/01/24 01:46:48  ddiego
-*added a cvs "log" comment to the top of all files in vcf/src and vcf/include
-*to facilitate change tracking
-*
-*/
-
-
 #endif // _VCF_DISTRIBUTEDCLASSREGISTRY_H__
 
-
+/**
+$Id$
+*/
