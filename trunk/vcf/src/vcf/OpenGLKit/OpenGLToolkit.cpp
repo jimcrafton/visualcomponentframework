62a8e5a6aa83ed99e7593a4bdc1611814e65180e
kdmix <kdmix@cccfda39-7811-0410-ae5f-bc4b32ab4073>
2006-04-28 17:31:05 +0000

    CVS Log info removed. Id keyword added.

--- trunk/vcf/src/vcf/OpenGLKit/OpenGLToolkit.cpp
+++ trunk/vcf/src/vcf/OpenGLKit/OpenGLToolkit.cpp
@@ -26,22 +26,5 @@ OpenGLPeer *OpenGLToolkit::createOpenGLPeer(GraphicsContext *context, OpenGLCont
 
 
 /**
-*CVS Log info
-*$Log$
-*Revision 1.3  2004/12/01 04:31:45  ddiego
-*merged over devmain-0-6-6 code. Marcello did a kick ass job
-*of fixing a nasty bug (1074768VCF application slows down modal dialogs.)
-*that he found. Many, many thanks for this Marcello.
-*
-*Revision 1.2.2.1  2004/10/27 22:42:47  augusto_roman
-*Changed Win32 peer to create GL Rendering Context (RC) based off of the Win32 window handle of the control instead of the paintDC.  Also enforced error checking. - aroman
-*
-*Revision 1.2  2004/08/07 02:49:19  ddiego
-*merged in the devmain-0-6-5 branch to stable
-*
-*Revision 1.1.2.2  2004/06/06 07:05:34  marcelloptr
-*changed macros, text reformatting, copyright sections
-*
+$Id$
 */
-
-
