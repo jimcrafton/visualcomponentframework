21dad0a740f63a80d27b3d23678e54e6238fb1fb
ddiego <ddiego@cccfda39-7811-0410-ae5f-bc4b32ab4073>
2006-06-01 04:04:16 +0000

    added ellipse and circle drawing

--- trunk/vcf/examples/Gradients/Gradients.cpp
+++ trunk/vcf/examples/Gradients/Gradients.cpp
@@ -5,151 +5,9 @@
 #include "vcf/GraphicsKit/GradientFills.h"
 
 
-#include "thirdparty/common/agg/include/agg_ellipse.h"
-#include "thirdparty/common/agg/include/agg_bounding_rect.h"
-
 
 using namespace VCF;
 
-
-class Ellipse2 : public VCF::Object, public VCF::Path {
-public:
-	virtual void applyTransform( const Matrix2D& transform ) {
-		agg::trans_affine mat = transform;
-		agg::conv_transform< agg::path_storage > xfrmdPath(ellipsePath_,mat);
-
-		ellipsePath_.remove_all();
-		ellipsePath_.concat_path(xfrmdPath);
-	}
-
-	virtual bool contains( const Rect& rect) {
-		agg::rasterizer_scanline_aa<> rasterizer;
-		agg::conv_curve<agg::path_storage> smooth(ellipsePath_);
-		rasterizer.add_path( smooth );
-
-		Point topLeft = rect.getTopLeft();
-		Point bottomRight = rect.getTopLeft();
-
-		return rasterizer.hit_test( (unsigned int)bottomRight.x_, (unsigned int)bottomRight.y_ ) && rasterizer.hit_test( (unsigned int)topLeft.x_, (unsigned int)topLeft.y_ );
-	}
-
-    virtual bool contains( const Point& pt ) {
-		agg::rasterizer_scanline_aa<> rasterizer;
-		agg::conv_curve<agg::path_storage> smooth(ellipsePath_);
-		rasterizer.add_path( smooth );
-
-		return rasterizer.hit_test( (unsigned int)pt.x_, (unsigned int)pt.y_ );
-	}
-
-    virtual bool intersects( const Point& pt ) {
-		agg::rasterizer_scanline_aa<> rasterizer;
-		agg::conv_curve<agg::path_storage> smooth(ellipsePath_);
-		agg::conv_stroke<agg::conv_curve<agg::path_storage> >  stroke(smooth);
-		stroke.width( 2 );
-
-		rasterizer.add_path( stroke );
-		return rasterizer.hit_test( (unsigned int)pt.x_, (unsigned int)pt.y_ );
-	}
-
-    virtual bool intersects( const Rect& rect ) {
-		agg::rasterizer_scanline_aa<> rasterizer;
-		agg::conv_curve<agg::path_storage> smooth(ellipsePath_);
-		rasterizer.add_path( smooth );
-		
-		if ( rasterizer.hit_test( (unsigned int)rect.left_, (unsigned int)rect.top_ ) ) {
-			return true;
-		}
-		
-		if ( rasterizer.hit_test( (unsigned int)rect.right_, (unsigned int)rect.top_ ) ) {
-			return true;
-		}
-		
-		if ( rasterizer.hit_test( (unsigned int)rect.right_, (unsigned int)rect.bottom_ ) ) {
-			return true;
-		}
-		
-		if ( rasterizer.hit_test( (unsigned int)rect.left_, (unsigned int)rect.bottom_ ) ) {
-			return true;
-		}
-		
-		return false;
-	}
-
-	virtual Rect getBounds() {
-		Rect result;
-
-		uint32 pid[1] = {0};
-
-		agg::bounding_rect( ellipsePath_, pid, 0, 1,
-						&result.left_, &result.top_,
-						&result.right_, &result.bottom_ );
-
-		return result;
-	}
-
-	virtual WindingRule getWindingRule() {
-		return wrNonZero;
-	}
-
-	virtual void setWindingRule( WindingRule rule ) {
-
-	}
-
-	virtual bool getPoints( std::vector<PathPoint>& points, Matrix2D* transform ) {
-		agg::trans_affine mat;
-		if ( NULL != transform ) {
-			mat = *transform;
-		}
-		agg::conv_transform< agg::path_storage > xfrmdPath(ellipsePath_,mat);
-		
-		for ( size_t i=0;i<ellipsePath_.total_vertices();i++ ) {
-			PathPoint pt;
-			unsigned vertCmd = xfrmdPath.vertex(&pt.point_.x_, &pt.point_.y_);
-			switch ( vertCmd ) {
-				case agg::path_cmd_move_to : {
-					pt.type_ = PathPoint::ptMoveTo;
-				}
-				break;
-
-				case agg::path_cmd_line_to : {
-					pt.type_ = PathPoint::ptLineTo;
-				}
-				break;
-
-				default : {
-					if ( vertCmd & agg::path_flags_close ) {
-						pt.type_ = PathPoint::ptClose;
-					}
-				}
-				break;
-			}
-
-			points.push_back( pt );
-		}
-
-
-		return !points.empty();
-	}
-
-	virtual void flattenPoints( std::vector<Point>& flattenedPoints ) {
-
-	}
-
-
-	void ellipse( const Rect& bounds ) {
-		agg::ellipse ellipseShape( bounds.left_ + bounds.getWidth()/2.0, 
-									bounds.top_ + bounds.getHeight()/2.0,
-									bounds.getWidth(), 
-									bounds.getHeight(), 100 );
-
-		ellipsePath_.rewind(0);
-		ellipsePath_.remove_all();
-		ellipsePath_.concat_path( ellipseShape );
-
-	}
-protected:
-	agg::path_storage ellipsePath_;
-};
 
 class GradientsWindow : public Window {
 public:
@@ -205,19 +63,26 @@ public:
 
 
 		ConicGradientFill fill3;
-		fill3.setEnd( 150 );
-		fill3.setStart( 0 );
 		fill3.setGradientMatrix( Matrix2D::translation(100, 410) );
 		fill3.setColorValue( 0, Color("gold") );
 		fill3.setColorValue( 1, Color("red") );
 
 		ctx->setCurrentFill(&fill3);
 
-		Ellipse2 el;
+		VCF::Ellipse el;
 		el.ellipse( Rect(0,360,200,460) );
 
 		ctx->draw(&el);
 		
+
+		
+		fill3.setGradientMatrix( Matrix2D::translation(350, 410) );
+
+		VCF::Circle circ;
+		circ.circle( Point(350,410),100 );
+		ctx->draw(&circ);
+
+
 
 
 		VerticalGradientFill fill4a;
