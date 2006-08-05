360867aee2ad1252771b81f2d22a178482588265
iamfraggle <iamfraggle@cccfda39-7811-0410-ae5f-bc4b32ab4073>
2006-08-05 16:28:35 +0000

    Second pass, ascii only, don't try wide characters on it for now.

--- trunk/bluesky/RegExKit/onig_cpp.cpp
+++ trunk/bluesky/RegExKit/onig_cpp.cpp
@@ -1,4 +1,4 @@
-#include "oniguruma.h"
+//#include "oniguruma.h"
 #include "onig_cpp.h"
 
 namespace VCF {
@@ -6,7 +6,7 @@ namespace VCF {
 // Regex::Iterator
 
     Regex::Iterator& Regex::Iterator::operator++() {
-        return *this=host_->find(data_->getPos());
+        return *this=data_->getEnv()->find(data_->getPos());
     }
 
     Regex::Iterator Regex::Iterator::operator++(int) {
@@ -16,7 +16,7 @@ namespace VCF {
     }
 
     Regex::Iterator& Regex::Iterator::operator--() {
-        return *this=host_->rfind(data_->getPos());
+        return *this=data_->getEnv()->rfind(data_->getPos());
     }
 
     Regex::Iterator Regex::Iterator::operator--(int) {
@@ -25,56 +25,58 @@ namespace VCF {
         return result;
     }
 
-// Regex
-    Regex::Iterator Regex::find(ptrdiff_t pos) {
-        OnigErrorInfo error;
+// Regex::Host
+
+    Regex::Host::Host(const String& exp, unsigned char* f, unsigned char* l): expression_(exp), first_(f), last_(l) {
+        if (init()!=ONIG_NORMAL) System::println("Some sort of error");
+    }
+
+    Regex::Host::Host(const String& expression, const String& source): expression_(expression) {
+        first_=(unsigned char*)source.ansi_c_str();
+        last_=(unsigned char*)(source.ansi_c_str()+source.length()-1);
+        if (init()!=ONIG_NORMAL) System::println("Some sort of error");
+    }
+
+    int Regex::Host::init() {
+        return onig_new(&reg_, (unsigned char*)expression_.ansi_c_str(), (unsigned char*)expression_.ansi_c_str() + expression_.length() - 1,
+            ONIG_OPTION_DEFAULT, ONIG_ENCODING_ASCII, ONIG_SYNTAX_PERL, &error_);
+    }
+
+    Regex::Iterator Regex::Host::find(ptrdiff_t pos) {
         OnigRegion* region;
-        regex_t* reg;
         String matchtext;
         ptrdiff_t status;
 
-        status = onig_new(&reg, (unsigned char*)expression_.ansi_c_str(), (unsigned char*)expression_.ansi_c_str() + expression_.length(),
-            ONIG_OPTION_DEFAULT, ONIG_ENCODING_UTF8, ONIG_SYNTAX_PERL, &error);
-        if (status != ONIG_NORMAL) {
-            return Iterator(new Match(-1,"Regex compilation error"), this);
-        }
         region = onig_region_new();
-        status = onig_search(reg, first_, last_, first_+pos, last_, region, ONIG_OPTION_NONE);
+        status = onig_search(reg_, first_, last_, first_+pos, last_, region, ONIG_OPTION_NONE);
         if (status>=0) {
-            matchtext=String((WideChar*)region->beg,(region->end-region->beg)/sizeof(WideChar));
+            matchtext=String((char*)region->beg,(region->end-region->beg)/sizeof(char));
             onig_region_free(region,1);
-            return Iterator(new Match(status,matchtext),this);
+            return Iterator(new Match(status, matchtext, this));
         } else {
             onig_region_free(region,1);
-            return Iterator(new Match(-1,"No match found"), this);
+            return Iterator(new Match(-1, "No match found", this));
         }
     }
 
-    Regex::Iterator Regex::rfind(ptrdiff_t pos){
-        OnigErrorInfo error;
+    Regex::Iterator Regex::Host::rfind(ptrdiff_t pos){
         OnigRegion* region;
-        regex_t* reg;
         String matchtext;
         ptrdiff_t status;
 
-        status = onig_new(&reg, (unsigned char*)expression_.ansi_c_str(), (unsigned char*)expression_.ansi_c_str() + expression_.length(),
-            ONIG_OPTION_DEFAULT, ONIG_ENCODING_UTF8, ONIG_SYNTAX_PERL, &error);
-        if (status != ONIG_NORMAL) {
-            return Iterator(new Match(-1,"Regex compilation error"), this);
-        }
         region = onig_region_new();
-        status = onig_search(reg, first_, last_, first_+pos, first_, region, ONIG_OPTION_NONE);
+        status = onig_search(reg_, first_, last_, first_+pos, first_, region, ONIG_OPTION_NONE);
         if (status>=0) {
             matchtext=String((WideChar*)region->beg, (region->end-region->beg)/sizeof(WideChar));
             onig_region_free(region,1);
-            return Iterator(new Match(status,matchtext),this);
+            return Iterator(new Match(status, matchtext, this));
         } else {
             onig_region_free(region,1);
-            return Iterator(new Match(-1,"No match found"), this);
+            return Iterator(new Match(-1, "No match found", this));
         }
     }
 
-    Regex::MatchList Regex::findAll() {
+    Regex::MatchList Regex::Host::findAll() {
         MatchList result;
         Iterator it=begin();
         while (it->getPos() != -1) {
--- trunk/bluesky/RegExKit/onig_cpp.h
+++ trunk/bluesky/RegExKit/onig_cpp.h
@@ -1,93 +1,101 @@
 //#include <vector>
+#include "oniguruma.h"
 #include "vcf/FoundationKit/FoundationKit.h"
 
-namespace VCF {
+namespace VCF{
+namespace Regex{
+
+    class Host;
+
+    class Match {
+        public:
+            Match(ptrdiff_t pos, String text, Host* const env): pos_(pos), matchText_(text), env_(env){};
+            Match(const Match& rhs): pos_(rhs.pos_), matchText_(rhs.matchText_), env_(rhs.env_){}
+            Match operator=(const Match& rhs){
+                return Match(rhs);
+            }
+
+            const ptrdiff_t& getPos(){
+                return pos_;
+            }
+
+            const String& getText(){
+                return matchText_;
+            }
+
+            Host* const getEnv(){
+                return env_;
+            }
+        private:
+            ptrdiff_t pos_;
+            String matchText_;
+            Host* const env_;
+    };
+
+    typedef std::vector<Match> MatchList;
+
+    class Iterator: public std::iterator<std::bidirectional_iterator_tag, Match> {
+        public:
+            // trivial
+            Iterator(Match* data): data_(data) {}
+            ~Iterator(){
+                delete data_;
+            }
+
+            Match& operator*(){
+                return *data_;
+            }
+
+            Match* operator->(){
+                return data_;
+            }
+            // input/forward
+            Iterator& operator++();
+            Iterator operator++(int);
+            // bidirectional
+            Iterator& operator--();
+            Iterator operator--(int);
+        private:
+            Match* data_;
+    };
+
+    typedef std::reverse_iterator<Iterator> ReverseIterator;
 
 
+    class Host {
+        public:
+            Host(const String& expression, unsigned char* first, unsigned char* last);
+            Host(const String& expression, const String& source);
+            Iterator find(ptrdiff_t pos);
+            Iterator find(Iterator it) {
+                return find(it->getPos());
+            }
+            Iterator rfind(ptrdiff_t pos);
+            Iterator rfind(Iterator it) {
+                return rfind(it->getPos());
+            }
+            MatchList findAll();
+            Iterator begin(){
+                return find(0);
+            }
+            Iterator end(){
+                return Iterator(NULL);
+            }
+            ReverseIterator rbegin(){
+                return ReverseIterator(rfind(Iterator(new Match(last_-first_, "", this))));
+            }
+            ReverseIterator rend(){
+                return ReverseIterator(Iterator(NULL));
+            }
+        private:
+            int init();
 
-class Regex {
+            const String expression_;
+            unsigned char* first_;
+            unsigned char* last_;
+            regex_t* reg_;
+            OnigErrorInfo error_;
+    };
 
-    public:
-
-        class Match {
-            public:
-                Match(ptrdiff_t pos, String text): pos_(pos), matchText_(text) {};
-
-                const ptrdiff_t& getPos(){
-                    return pos_;
-                }
-
-                const String& getText(){
-                    return matchText_;
-                }
-            private:
-                ptrdiff_t pos_;
-                String matchText_;
-        };
-
-        typedef std::vector<Match> MatchList;
-
-        class Iterator: public std::iterator<std::bidirectional_iterator_tag, Match> {
-            public:
-                // trivial
-                Iterator(Match* data, Regex* host): data_(data), host_(host) {}
-                ~Iterator(){
-                    delete data_;
-                }
-
-                Match& operator*(){
-                    return *data_;
-                }
-
-                Match* operator->(){
-                    return data_;
-                }
-                // input/forward
-                Iterator& operator++();
-                Iterator operator++(int);
-                // bidirectional
-                Iterator& operator--();
-                Iterator operator--(int);
-            private:
-                Match* data_;
-                Regex* host_;
-        };
-
-        typedef std::reverse_iterator<Iterator> ReverseIterator;
-
-    public:
-        Regex(const String& expression, unsigned char* first, unsigned char* last):
-            expression_(expression), first_(first), last_(last) {}
-/*        Regex(const String& expression, const String& source):
-            expression_(expression) {
-            first_=source.begin();
-            last_=source.end();
-        }*/
-        Iterator find(ptrdiff_t pos);
-        Iterator find(Iterator it) {
-            return find(it->getPos());
-        }
-        Iterator rfind(ptrdiff_t pos);
-        Iterator rfind(Iterator it) {
-            return rfind(it->getPos());
-        }
-        MatchList findAll();
-        Iterator begin(){
-            return find(0);
-        }
-        Iterator end(){
-            return Iterator(NULL, this);
-        }
-        ReverseIterator rbegin(){
-            return ReverseIterator(rfind(Iterator(new Match(last_-first_, ""), this)));
-        }
-        ReverseIterator rend(){
-            return ReverseIterator(Iterator(NULL, this));
-        }
-    private:
-        String expression_;
-        unsigned char* first_;
-        unsigned char* last_;
-};
-
+}
 }
