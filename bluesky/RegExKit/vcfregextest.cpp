d3d77b629597edd92aeaa6d73f93b0c4e440bf6b
iamfraggle <iamfraggle@cccfda39-7811-0410-ae5f-bc4b32ab4073>
2006-08-15 19:49:29 +0000

    UTF_16LE capability added to RegExKit (doesn't yet handle 4 byte characters correctly)

--- bluesky/RegExKit/vcfregextest.cpp
+++ bluesky/RegExKit/vcfregextest.cpp
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
     String target="This is a test. Do not panic. I repeat: DO NOT PANIC!";
     String exp="is";
 
-    Regex::Host engine(exp, target);
+    Regex::Ascii engine(exp, target);
     System::println(Format("Test string: %s") % target);
     Regex::Iterator it=engine.begin();
     while(it!=(engine.end())){
@@ -34,6 +34,15 @@ int main(int argc, char *argv[])
         System::println(Format("Match found for '%s' at position %d") % rit->getText() % rit->getPos());
     }
     while(rit!=(engine.rbegin()));
+
+	System::println("Starting again, from the back, in Unicode");
+	exp="t";
+	Regex::UTF_16LE engine2(exp, target);
+	it=--(engine2.end());
+	System::println(Format("Found '%ls' at position %d") % it->getText() % it->getPos());
+	--it;
+	System::println(Format("and again at position %d") % it->getPos());
+	System::println("and I can't be bothered to look anymore");
 
 	FoundationKit::terminate();
 
