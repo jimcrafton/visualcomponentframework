2707d6e3e19350629573bff6e33c768ade16b9b6
mispunt <mispunt@cccfda39-7811-0410-ae5f-bc4b32ab4073>
2006-08-28 17:03:46 +0000

    - Changed the size of the window, so all the buttons are vissible. - Code::Blocks has stript all the unneeded spaces and tabs. (at the end of a row)

--- vcf/examples/Dialogs/Dialogs.cpp
+++ vcf/examples/Dialogs/Dialogs.cpp
@@ -14,11 +14,11 @@ This example demonstrates how to use dialogs
 
 
 /**
-This demonstrates how to create a very simple 
-dialog complete with OK/Cancel buttons, and  
-horizontal layout that takes into consideration 
+This demonstrates how to create a very simple
+dialog complete with OK/Cancel buttons, and
+horizontal layout that takes into consideration
 the proper (as specified in the UI metrics for the
-platform) horizontal and vertial spacing between 
+platform) horizontal and vertial spacing between
 controls.
 */
 class MyDialog : public Dialog {
@@ -31,17 +31,17 @@ public:
 		Panel* pane1 = new Panel();
 		pane1->setBorder( NULL );
 
-		HorizontalLayoutContainer* container = new HorizontalLayoutContainer();		
+		HorizontalLayoutContainer* container = new HorizontalLayoutContainer();
 		pane1->setContainer( container );
 
-		
+
 
 		Label* lable1 = new Label();
 		lable1->setCaption( "Name:" );
 		pane1->add( lable1 );
 
 		TextControl* edit1 = new TextControl();
-		
+
 		pane1->add( edit1 );
 
 
@@ -61,21 +61,21 @@ public:
 		bottom->setBorder( NULL );
 
 		bottom->setHeight( okBtn->getPreferredHeight() + UIToolkit::getUIMetricValue( UIMetricsManager::mtContainerBorderDelta) * 2 );
-		
+
 
 		HorizontalLayoutContainer* container2 = new HorizontalLayoutContainer();
-		container2->setLeftBorderWidth( getWidth() / 2.0 );	
+		container2->setLeftBorderWidth( getWidth() / 2.0 );
 
 		double width = getWidth() / 2.0;
 		width -= UIToolkit::getUIMetricValue(UIMetricsManager::mtContainerBorderDelta);
 		width -= container2->getColumnTweenWidth( 0 );
 
 		container2->setColumnWidth( 0, width / 2.0 );
-		
+
 		bottom->setContainer( container2 );
 
 
-		
+
 		bottom->add( okBtn );
 
 		CommandButton* cancelBtn = new CommandButton();
@@ -107,7 +107,7 @@ public:
 		CommandButton* btn1 = new CommandButton();
 		btn1->setBounds( 10, 20, 175, btn1->getPreferredHeight() );
 		btn1->setCaption( "Dialog::showMessage 1" );
-		btn1->ButtonClicked += 
+		btn1->ButtonClicked +=
 			new GenericEventHandler<DialogsWindow>( this, &DialogsWindow::example1, "DialogsWindow::example1" );
 		add( btn1 );
 
@@ -115,7 +115,7 @@ public:
 		CommandButton* btn2 = new CommandButton();
 		btn2->setBounds( 10, btn1->getBottom() + 20, 175, btn2->getPreferredHeight() );
 		btn2->setCaption( "Dialog::showMessage 2" );
-		btn2->ButtonClicked += 
+		btn2->ButtonClicked +=
 			new GenericEventHandler<DialogsWindow>( this, &DialogsWindow::example2, "DialogsWindow::example2" );
 		add( btn2 );
 
@@ -123,49 +123,49 @@ public:
 		CommandButton* btn3 = new CommandButton();
 		btn3->setBounds( 10, btn2->getBottom() + 20, 175, btn3->getPreferredHeight() );
 		btn3->setCaption( "Show custom dialog" );
-		btn3->ButtonClicked += 
+		btn3->ButtonClicked +=
 			new GenericEventHandler<DialogsWindow>( this, &DialogsWindow::example3, "DialogsWindow::example3" );
 		add( btn3 );
 
 		CommandButton* btn4 = new CommandButton();
 		btn4->setBounds( 10, btn3->getBottom() + 20, 175, btn4->getPreferredHeight() );
 		btn4->setCaption( "Show Common Font Dialog" );
-		btn4->ButtonClicked += 
+		btn4->ButtonClicked +=
 			new GenericEventHandler<DialogsWindow>( this, &DialogsWindow::example4, "DialogsWindow::example4" );
 		add( btn4 );
 
 		CommandButton* btn5 = new CommandButton();
 		btn5->setBounds( 10, btn4->getBottom() + 20, 175, btn4->getPreferredHeight() );
 		btn5->setCaption( "Show Common Color Dialog" );
-		btn5->ButtonClicked += 
+		btn5->ButtonClicked +=
 			new GenericEventHandler<DialogsWindow>( this, &DialogsWindow::example5, "DialogsWindow::example5" );
 		add( btn5 );
 
 		CommandButton* btn6 = new CommandButton();
 		btn6->setBounds( 10, btn5->getBottom() + 20, 175, btn6->getPreferredHeight() );
 		btn6->setCaption( "Show Common File Browse Dialog" );
-		btn6->ButtonClicked += 
+		btn6->ButtonClicked +=
 			new GenericEventHandler<DialogsWindow>( this, &DialogsWindow::example6, "DialogsWindow::example6" );
 		add( btn6 );
 
 		CommandButton* btn7 = new CommandButton();
 		btn7->setBounds( 10, btn6->getBottom() + 20, 175, btn6->getPreferredHeight() );
 		btn7->setCaption( "Show Common File Open Dialog" );
-		btn7->ButtonClicked += 
+		btn7->ButtonClicked +=
 			new GenericEventHandler<DialogsWindow>( this, &DialogsWindow::example7, "DialogsWindow::example7" );
 		add( btn7 );
 
 		CommandButton* btn8 = new CommandButton();
 		btn8->setBounds( 10, btn7->getBottom() + 20, 175, btn6->getPreferredHeight() );
 		btn8->setCaption( "Show Common File Save Dialog" );
-		btn8->ButtonClicked += 
+		btn8->ButtonClicked +=
 			new GenericEventHandler<DialogsWindow>( this, &DialogsWindow::example8, "DialogsWindow::example8" );
 		add( btn8 );
 
 		CommandButton* btn9 = new CommandButton();
 		btn9->setBounds( 10, btn8->getBottom() + 20, 175, btn6->getPreferredHeight() );
 		btn9->setCaption( "Show Common Print Dialog" );
-		btn9->ButtonClicked += 
+		btn9->ButtonClicked +=
 			new GenericEventHandler<DialogsWindow>( this, &DialogsWindow::example9, "DialogsWindow::example9" );
 		add( btn9 );
 	}
@@ -175,7 +175,7 @@ public:
 	/**
 	This example will deomonstrate how to display a modal, information dialog,
 	to present some information to a user.
-	All you have to pass in is the caption - the title of the dialog will be assigned 
+	All you have to pass in is the caption - the title of the dialog will be assigned
 	the name of the application
 	*/
 	void example1( Event* e ) {
@@ -226,27 +226,27 @@ public:
 
 	/**
 	This example also displays a modal dialog. You can specify more options
-	such as the type of icon to display, the title of the dialog, and the 
+	such as the type of icon to display, the title of the dialog, and the
 	kind of buttons the user can respond with.
 	*/
 	void example2( Event* e ) {
 		UIToolkit::ModalReturnType result;
 
-		result = Dialog::showMessage( "Hello - this is an example using \nOK and Cancel buttons.", 
+		result = Dialog::showMessage( "Hello - this is an example using \nOK and Cancel buttons.",
 										"Fluffaluffagus",
 										Dialog::mbOKCancel,
 										Dialog::msError );
 
 		Dialog::showMessage( "You selected: " + modalReturnToString( result ) );
 
-		result = Dialog::showMessage( "Hello - this is an example using \nYes and No buttons.", 
+		result = Dialog::showMessage( "Hello - this is an example using \nYes and No buttons.",
 										"Fluffaluffagus",
 										Dialog::mbYesNo,
 										Dialog::msWarning );
 
 		Dialog::showMessage( "You selected: " + modalReturnToString( result ) );
 
-		result = Dialog::showMessage( "Hello - this is an example using \nAbort, Retry and Ignore buttons.", 
+		result = Dialog::showMessage( "Hello - this is an example using \nAbort, Retry and Ignore buttons.",
 										"Format your Hard Disk?",
 										Dialog::mbAbortRetryIgnore,
 										Dialog::msInfo );
@@ -285,7 +285,7 @@ public:
 
 	/**
 	This example shows how to call up the common Color dialog
-	*/	
+	*/
 	void example5( Event* e ) {
 		CommonColorDialog dlg(this);
 		dlg.setSelectedColor( getColor() );
@@ -303,7 +303,7 @@ public:
 	*/
 	void example6( Event* e ) {
 		CommonFileBrowseDialog dlg(this);
-		
+
 		dlg.setDirectory( System::getCurrentWorkingDirectory() );
 
 		if ( dlg.execute() ) {
@@ -316,7 +316,7 @@ public:
 	*/
 	void example7( Event* e ) {
 		CommonFileOpenDialog dlg(this);
-		
+
 		dlg.addFilter( "Pumpernikel Bread", "*.pmk" );
 		if ( dlg.execute() ) {
 
@@ -328,7 +328,7 @@ public:
 	*/
 	void example8( Event* e ) {
 		CommonFileSaveDialog dlg(this);
-		
+
 		if ( dlg.execute() ) {
 
 		}
@@ -339,7 +339,7 @@ public:
 	*/
 	void example9( Event* e ) {
 		CommonPrintDialog dlg(this);
-		
+
 		if ( dlg.execute() ) {
 
 		}
@@ -358,12 +358,12 @@ public:
 
 	virtual bool initRunningApplication(){
 		bool result = Application::initRunningApplication();
-		
+
 		Window* mainWindow = new DialogsWindow();
 		setMainWindow(mainWindow);
-		mainWindow->setBounds( &Rect( 100.0, 100.0, 500.0, 500.0 ) );
+		mainWindow->setBounds( &Rect( 100.0, 100.0, 300.0, 540.0 ) );
 		mainWindow->show();
-		
+
 		return result;
 	}
 
@@ -375,7 +375,7 @@ int main(int argc, char *argv[])
 	Application* app = new DialogsApplication( argc, argv );
 
 	Application::main();
-	
+
 	return 0;
 }
 
