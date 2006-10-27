973dae2eacfe74cf40f1c74bc3e2b05c3bdd23ab
ddiego <ddiego@cccfda39-7811-0410-ae5f-bc4b32ab4073>
2006-10-27 04:31:11 +0000

    changes to net kit.

--- bluesky/NetworkKit/NetworkKit.cpp
+++ bluesky/NetworkKit/NetworkKit.cpp
@@ -6,6 +6,16 @@
 #include "vcf/FoundationKit/FoundationKit.h"
 #include "vcf/FoundationKit/Dictionary.h"
 
+#include "vcf/FoundationKit/StringTokenizer.h"
+
+#if (_WIN32_WINNT >= 0x0400)
+#pragma message ("_WIN32_WINNT >= 0x0400")
+#endif
+
+#if (_WIN32_WINDOWS >= 0x0400)
+#pragma message ("_WIN32_WINDOWS >= 0x0400")
+#endif
+
 
 namespace VCF {
 
@@ -566,12 +576,12 @@ namespace VCF {
 		/**
 		Seeking is not permitted!
 		*/
-		virtual void seek(const unsigned long& offset, const SeekType& offsetFrom) {
+		virtual void seek(const uint64& offset, const SeekType& offsetFrom) {
 			//no-op
 			throw NetworkException( "You cannot seek in a Socket Input stream" );
 		}
 
-		virtual unsigned long getSize() {
+		virtual uint64 getSize() {
 			return totalBytesRecvd_;
 		}
 
@@ -584,7 +594,7 @@ namespace VCF {
 		}
 
 		
-		virtual ulong32 getCurrentSeekPos() {
+		virtual uint64 getCurrentSeekPos() {
 			throw NetworkException( "You cannot access the seek position in a Socket Input stream" );
 			return 0;	
 		}
@@ -611,10 +621,10 @@ namespace VCF {
 		\li If the socket's peer reports an error in reading  
 		from the socket, then the method will throw a NetworkException.
 		*/
-		virtual unsigned long read( unsigned char* bytesToRead, unsigned long sizeOfBytes );
+		virtual uint64 read( unsigned char* bytesToRead, uint64 sizeOfBytes );
 
 	protected:
-		unsigned long totalBytesRecvd_;
+		uint64 totalBytesRecvd_;
 		Socket* socket_;
 	};
 
@@ -628,12 +638,12 @@ namespace VCF {
 		/**
 		Seeking is not permitted!
 		*/
-		virtual void seek(const unsigned long& offset, const SeekType& offsetFrom) {
+		virtual void seek(const uint64& offset, const SeekType& offsetFrom) {
 			//no-op
 			throw NetworkException( "You cannot seek in a Socket Output stream" );
 		}
 
-		virtual unsigned long getSize() {
+		virtual uint64 getSize() {
 			return totalBytesWritten_;
 		}
 
@@ -646,7 +656,7 @@ namespace VCF {
 		}
 
 		
-		virtual ulong32 getCurrentSeekPos() {
+		virtual uint64 getCurrentSeekPos() {
 			throw NetworkException( "You cannot access the seek position in a Socket Output stream" );
 			return 0;	
 		}
@@ -667,10 +677,10 @@ namespace VCF {
 		\li If the socket's peer reports an error in writing to 
 		the socket, then the method will throw a NetworkException.
 		*/
-		virtual unsigned long write( const unsigned char* bytesToWrite, unsigned long sizeOfBytes );
+		virtual uint64 write( const unsigned char* bytesToWrite, uint64 sizeOfBytes );
 
 	protected:
-		unsigned long totalBytesWritten_;
+		uint64 totalBytesWritten_;
 		Socket* socket_;
 	};
 
@@ -688,9 +698,9 @@ SocketInputStream::SocketInputStream( Socket& socket ):
 }
 
 
-unsigned long SocketInputStream::read( unsigned char* bytesToRead, unsigned long sizeOfBytes )
+uint64 SocketInputStream::read( unsigned char* bytesToRead, uint64 sizeOfBytes )
 {
-	unsigned long bytesRead = 0;
+	uint64 bytesRead = 0;
 
 	int err = socket_->getPeer()->recv( bytesToRead, sizeOfBytes );
 	if ( err >= 0 ) {
@@ -715,9 +725,9 @@ SocketOutputStream::SocketOutputStream( Socket& socket ):
 
 }
 
-unsigned long SocketOutputStream::write( const unsigned char* bytesToWrite, unsigned long sizeOfBytes )
+uint64 SocketOutputStream::write( const unsigned char* bytesToWrite, uint64 sizeOfBytes )
 {
-	unsigned long bytesWritten = 0;
+	uint64 bytesWritten = 0;
 
 	int err = socket_->getPeer()->send( bytesToWrite, sizeOfBytes );
 
@@ -1958,10 +1968,174 @@ using namespace VCF;
 
 class ReadThread : public Thread {
 public:
+	Socket* socket;
+	ReadThread( Socket* s ) : socket(s){
+
+	}
+
+
+	AnsiString msgData;
+
+	void doMsg() {
+		
+		System::println( "-------------------------------------------------------------------------------" );
+
+		String msg = msgData;
+
+		StringTokenizer tok( msg, " ");
+
+		int elementCount = 0;
+
+		char senderNick[256];
+		char senderUser[256];
+		char senderHost[256];
+
+		memset(senderNick,0,sizeof(senderNick));
+		memset(senderUser,0,sizeof(senderUser));
+		memset(senderHost,0,sizeof(senderHost));
+
+
+		std::vector<String> params;
+
+		int offset = 0;
+
+		while ( tok.hasMoreElements() ) {
+			String s = tok.nextElement();
+			
+
+			if ( 0 == elementCount && s[0] == ':' ) {							
+				
+				
+				if ( tok.hasMoreElements() ) {					
+					sscanf( s.ansi_c_str() + 1, "%[^!]!%[^@]@%s", senderNick, senderUser, senderHost );
+					
+					offset += s.size() + 1;
+
+					s = tok.nextElement();
+
+					params.push_back( s );
+
+					offset += s.size() + 1;
+					elementCount ++;
+					continue;
+				}
+				
+			}
+			else {
+				int a = 0;
+				int d = 0;
+				if ( 0 == d ) {
+					if ( s[0] == ':' ) {
+						s.erase(0,1);
+						d = 1;
+					}
+					
+
+					a++;
+
+					if ( 0 == d ) {
+						params.push_back( s );						
+					}
+					else {						
+						
+						params.push_back( msgData.substr(offset,msgData.size()-offset) );
+						break;
+					}
+				}				
+			}
+			
+			offset += s.size() + 1;
+			elementCount ++;
+		}
+
+		System::println( "Msg:<<" );
+
+		System::println( Format( "\tnick:%s") % senderNick );
+		System::println( Format( "\tuser:%s") % senderUser );
+		System::println( Format( "\thost:%s") % senderHost );
+
+		
+
+		std::vector<String>::iterator it = params.begin();
+		while ( it != params.end() ) {
+
+			System::println( Format( "\tparam:%s") % *it );
+
+			it++;
+		}
+
+
+		System::println( ">>" );
+
+	}
+
+	void parseMsg( const uchar* buf, size_t size ) {
+		const uchar* start = buf;
+		const uchar* tmp = buf;
+		while ( ((tmp-buf) < size) ) {
+			if ( (*tmp == '\r') || (*tmp == '\n') ) {
+
+				msgData.append( (const char*)start, tmp-start );
+				
+				doMsg();
+
+
+				msgData = "";
+
+				if ( *tmp == '\r' ) {
+					tmp ++;
+				}
+
+				if ( *tmp == '\n' ) {
+					tmp ++;
+				}
+
+				start = tmp;
+			}
+			else {
+				tmp++;
+			}
+		}
+
+		if ( (tmp - start) > 1 ) {
+			msgData.append( (const char*)start, tmp-start );
+		}
+
+	}
+
 	virtual bool run() {
+
+		SocketInputStream sis(*socket);
+
+		uchar tmp[256];
+		while ( canContinue() ) {
+			socket->getPeer()->select( Socket::SelectWaitForever, NULL, NULL, NULL );
+
+			do {
+				try {
+					uint64 err = sis.read( tmp, sizeof(tmp) );
+					
+					if ( err == 0 ) {
+						System::println( "Disconnect!" );
+						break;
+					}
+					else {
+						tmp[err] = 0;
+						parseMsg( tmp, err );						
+					}
+				}
+				catch ( BasicException& ) {
+					System::println( "Socket error!" );
+					break;
+				}
+			} while( socket->pending() && socket->isReadable() );
+
+		}
+
 		return true;
 	}
 };
+
 
 
 class ServerThread : public Thread {
@@ -1988,6 +2162,99 @@ public:
 };
 
 
+#define VCF_ADD_TIMER		WM_APP + 1221
+/*
+class RThread : public Thread {
+public:
+	Delegate Perform;
+
+	std::vector<HANDLE> timerHandles;
+
+
+	static void CALLBACK TimerProc( HWND hwnd, UINT uMsg, UINT idEvent,  DWORD dwTime ) {
+		printf( "msg: %d, ev: %d, time: %d\n", uMsg, idEvent, dwTime );
+	}
+
+	void quit() {
+		PostThreadMessage( getPeer()->getThreadID(), WM_QUIT, 0, 0 );
+	}
+
+	int addTimer( uint32 ms ) {
+
+		HANDLE timer = ::CreateWaitableTimer( NULL, FALSE, NULL );
+
+		timerHandles.push_back(timer);
+
+		LARGE_INTEGER timeOut = {0};
+		timeOut.QuadPart = 0;
+		DateTime dueTime = System::convertLocalTimeToUTCTime( DateTime::now() );
+
+		dueTime.incrMilliSecond( ms );
+
+		SYSTEMTIME st;
+		st.wDay = dueTime.getDay();
+		st.wHour = dueTime.getHour();
+		st.wMinute = dueTime.getMinute();
+		st.wSecond = dueTime.getSecond();
+		st.wMilliseconds = dueTime.getMillisecond();
+		st.wDayOfWeek = dueTime.getWeekDay();
+		st.wMonth = dueTime.getMonth();
+		st.wYear = dueTime.getYear();
+		
+		SystemTimeToFileTime( &st, (FILETIME*)&timeOut );
+
+		SetWaitableTimer( timer, &timeOut, ms, NULL, NULL, FALSE );
+
+		
+
+		return res;
+	}
+
+
+
+	virtual bool run() {
+
+		
+		while ( canContinue() ) {
+			MSG msg = {0};
+
+			PeekMessage( &msg, NULL, 0, 0, PM_NOREMOVE );
+
+			Event e(this);
+			Perform.fireEvent( &e );
+
+
+			
+			if ( GetMessage( &msg, NULL, 0, 0 ) >= 0 ) {
+				switch ( msg.message ) {
+					case WM_QUIT : {
+						return true;
+					}
+					break;	
+					
+					case WM_TIMER : {
+						printf( "msg: %d, ev: %d\n", msg.message, msg.wParam );
+					}
+					break;
+
+					case VCF_ADD_TIMER : {
+						SetTimer( NULL, 101239, msg.wParam, RThread::TimerProc );
+					}
+					break;
+				}
+
+				TranslateMessage(&msg); 
+
+				DispatchMessage(&msg);
+			}
+
+		}
+
+		return true;
+	}
+};
+*/
+
 
 int main( int argc, char** argv ){
 
@@ -1995,13 +2262,20 @@ int main( int argc, char** argv ){
 
 	NetworkKit::init(  argc, argv );
 	
+
+	//RThread* rt = new RThread();
+	//rt->start();
+
+	//Sleep(1000);
+	//rt->addTimer(1000);
+
 	try {
 
 		IPAddress::RawBytes ip(4);
-		ip[0] = 123;
-		ip[1] = 168;
-		ip[2] = 23;
-		ip[3] = 103;
+		ip[0] = 127;
+		ip[1] = 0;
+		ip[2] = 0;
+		ip[3] = 1;
 
 		IPAddress addr(ip);
 
@@ -2017,15 +2291,43 @@ int main( int argc, char** argv ){
 		System::println( Format("host address: %s") % addr2.getHostAddress() );
 		System::println( Format("host name: %s") % addr2.getHostName() );
 
-		System::println( Format("host address: %s") % 	addr3.getHostAddress() );
-		System::println( Format("host name: %s") % addr3.getHostName() );
+		//System::println( Format("host address: %s") % 	addr3.getHostAddress() );
+		//System::println( Format("host name: %s") % addr3.getHostName() );
+
+
+		Socket ircServer;
+		unsigned short ircPort = 6667;
+		String ircHost = "irc.freenode.net";
+
+		ircServer.connect( ircHost, ircPort );
+
+		ReadThread* th = new ReadThread(&ircServer);
+		th->start();
+		
+		AnsiString msg;
+		msg = (String)(Format( "USER %s %s %s :%s" ) % "ddiego" % "b" % "c" % "Odysseus");
+		msg += " \r\n";
+
+		SocketOutputStream sos(ircServer);
+		sos.write( (const uchar*)msg.c_str(), msg.size() );
+
+		msg = (String)(Format( "NICK %s" ) % "Unknown[1]");
+		msg += " \r\n";
+		sos.write( (const uchar*)msg.c_str(), msg.size() );
+
+
+		//rt->quit();
 
 
 
-		IPAddress addr4("sdkfdsf");
-		addr4.getHostName();
+	//b_server_printf( server, "NICK %s", server->nickname );
+
+		while ( true ) {
+			Sleep(1000);
+		}
 
 
+/*
 		Socket server;
 		Dictionary options;
 		options[Socket::soBlocking] = false;
@@ -2047,8 +2349,6 @@ int main( int argc, char** argv ){
 			}
 			else {
 				
-				System::sleep( 100 );
-
 				SocketArray readList = clients;	
 				SocketArray writeList = clients;
 				SocketArray errorList = clients;
@@ -2089,6 +2389,8 @@ int main( int argc, char** argv ){
 				}
 			}
 		}
+		*/
+
 	}
 	catch ( BasicException& e ) {
 		System::println( e.getMessage() );
@@ -2099,5 +2401,8 @@ int main( int argc, char** argv ){
 	FoundationKit::terminate();
 	return 0;
 }
+
+
+
 
 
