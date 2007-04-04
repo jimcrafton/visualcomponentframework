861477b40c1ff612068578cb92c404f51e16eed8
obirsoy <obirsoy@cccfda39-7811-0410-ae5f-bc4b32ab4073>
2007-04-04 21:07:25 +0000

    . Linux RunLoop improvements.

---
+++ trunk/vcf/src/vcf/FoundationKit/AtomicCount.h
@@ -0,0 +1,23 @@
+#ifndef _VCF_ATOMICOUNT_H__
+#define _VCF_ATOMICOUNT_H__
+
+//AtomicCount.h
+
+/*
+Copyright 2000-2007 The VCF Project, Orhun Birsoy.
+Please see License.txt in the top level directory
+where you installed the VCF.
+*/
+
+#include <thirdparty/common/boost/detail/atomic_count.hpp>
+
+namespace VCF {
+	typedef boost::detail::atomic_count AtomicCount;
+};
+
+#endif //_VCF_ATOMICOUNT_H__
+
+/**
+$Id:$
+*/
+
