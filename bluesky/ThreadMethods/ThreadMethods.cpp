722a3e9c39993e91c8fa3de9532651b487c8a591
ddiego <ddiego@cccfda39-7811-0410-ae5f-bc4b32ab4073>
2007-05-28 04:13:27 +0000

    more delegate async invoke stuff. this pretty much wraps it up except for implementing more types for multiple arguments.

--- bluesky/ThreadMethods/ThreadMethods.cpp
+++ bluesky/ThreadMethods/ThreadMethods.cpp
@@ -300,7 +300,13 @@ int main( int argc, char** argv ){
 		printf( "d2 results[%d]: %d\n", i, (int)d3.results[i] );
 	}
 	
-	d3.beginInvoke( "Hola", 120.456, NULL );
+	ar = d3.beginInvoke( "Hola", 120.456, NULL );
+
+	ar->wait();
+
+	bool b = d3.endInvoke( ar );
+
+	ar->free();
 
 	/*
 	{
