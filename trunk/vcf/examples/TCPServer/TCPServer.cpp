ba0e60c4fc404cfb5e343a21b09ebf5b36a4353a
ddiego <ddiego@cccfda39-7811-0410-ae5f-bc4b32ab4073>
2007-10-11 04:22:40 +0000

    add tcp server example.

---
+++ trunk/vcf/examples/TCPServer/TCPServer.cpp
@@ -0,0 +1,87 @@
+////TCPServer.cpp
+
+#include "vcf/NetworkKit/NetworkKit.h"
+#include "vcf/NetworkKit/Socket.h"
+#include "vcf/NetworkKit/IPAddress.h"
+#include "vcf/FoundationKit/Dictionary.h"
+
+using namespace VCF;
+
+int main( int argc, char** argv ){
+
+	FoundationKit::init( argc, argv );
+	NetworkKit::init(  argc, argv );
+
+	try {		
+		TCPSocket server;
+		server.listen( 34251 );
+		
+		server.getRemotePort();
+		server.getLocalPort();
+
+		Dictionary options;
+		options[Socket::soBlocking] = false;
+		server.setOptions( options );
+
+		std::vector<Socket*> tmp;
+		std::vector<Socket*> clients;
+		while ( true ) {
+			System::println( "waiting..." );
+			tmp = clients;
+			if ( tmp.empty() ) {
+				server.selectFor( 5000, Socket::ssReadable );
+			}
+			else {
+				server.getPeer()->select( 5000, &tmp, NULL, NULL );
+			}
+			
+			TCPSocket* client = server.accept();
+			if ( NULL != client ) {
+				clients.push_back( client );
+
+				IPAddress addr = client->getRemoteHostIPAddress();
+				System::println( "Recv'd connect! From: " + addr.getHostAddress() +
+								" port: " + (int)client->getRemotePort() );
+			}
+
+
+			std::vector<Socket*>::iterator it = tmp.begin();
+			while ( it != tmp.end() ) {
+				Socket* s = *it;
+				TCPSocketInputStream sis( (TCPSocket&) *s );
+				unsigned char buf[256];
+				uint64 bytesRead = sis.read( &buf[0], sizeof(buf) );
+
+				IPAddress addr = s->getRemoteHostIPAddress();
+
+				if ( 0 == bytesRead ) {
+					
+					
+					System::println( "Lost connection from: " + addr.getHostAddress() +
+								" port: " + (int)s->getRemotePort() );
+
+					clients.erase( std::find(clients.begin(),clients.end(),s) );
+
+					s->free();
+				}
+				else {
+					System::println( Format("Recv'd %d bytes from: %s port %d") 
+										% bytesRead 
+										% addr.getHostAddress() 
+										% s->getRemotePort() );	
+				}
+
+				++it;
+			}
+
+		}
+	}
+	catch ( BasicException& e ) {
+		System::println( e.getMessage() );
+	}
+
+	NetworkKit::terminate();
+	FoundationKit::terminate();
+	return 0;
+}
+
