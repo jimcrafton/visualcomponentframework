ddb5549f6372bacb6fa97caa2abd2fe694688976
ddiego <ddiego@cccfda39-7811-0410-ae5f-bc4b32ab4073>
2007-11-30 23:49:05 +0000

    added visual c++ 9 project files.

---
+++ trunk/vcf/build/vc90/add-ins/MVCAppWizard/Templates/1033/MyView.cpp
@@ -0,0 +1,22 @@
+#include "vcf/ApplicationKit/ApplicationKit.h"
+#include "[!output PROJECT_NAME]View.h"
+#include "[!output PROJECT_NAME]Document.h"
+
+using namespace VCF;
+
+[!output PROJECT_NAME]View::[!output PROJECT_NAME]View()
+{
+
+}
+
+[!output PROJECT_NAME]View::~[!output PROJECT_NAME]View()
+{
+
+}
+
+void [!output PROJECT_NAME]View::paintView( VCF::GraphicsContext* ctx )
+{
+
+}
+
+
