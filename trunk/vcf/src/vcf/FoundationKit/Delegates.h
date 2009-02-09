6004ad07ebd88c548d651fc06827fba42d15b722
Incarnadine <Incarnadine@cccfda39-7811-0410-ae5f-bc4b32ab4073>
2009-02-09 07:19:09 +0000

    Call to Procedure6 should have been Procedure7.

--- trunk/vcf/src/vcf/FoundationKit/Delegates.h
+++ trunk/vcf/src/vcf/FoundationKit/Delegates.h
@@ -2774,7 +2774,7 @@ public:
 				break;
 
 				case 7 : {
-					Procedure7<P1,P2,P3,P4,P5,P6,P7>* callBack = (Procedure6<P1,P2,P3,P4,P5,P6,P7>*)cb;
+					Procedure7<P1,P2,P3,P4,P5,P6,P7>* callBack = (Procedure7<P1,P2,P3,P4,P5,P6,P7>*)cb;
 					callBack->invoke( p1, p2, p3, p4, p5, p6, p7 );
 				}
 				break;
