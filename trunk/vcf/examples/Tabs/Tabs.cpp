00384ed08761ca9ef1f3ccf9c488750cd3778ab9
ddiego <ddiego@cccfda39-7811-0410-ae5f-bc4b32ab4073>
2009-07-06 18:27:04 +0000

    minor mod to demo to show how to add custom tab page.

--- trunk/vcf/examples/Tabs/Tabs.cpp
+++ trunk/vcf/examples/Tabs/Tabs.cpp
@@ -3,6 +3,8 @@
 
 #include "vcf/ApplicationKit/ApplicationKit.h"
 #include "vcf/FoundationKit/RTTIMacros.h"
+#include "vcf/ApplicationKit/DefaultTabPage.h"
+#include "vcf/ApplicationKit/TabbedPages.h"
 
 using namespace VCF;
 
@@ -18,12 +20,45 @@ _class_rtti_end_
 
 
 
+class MyTabPage : public DefaultTabPage {
+public:
+	MyTabPage(): DefaultTabPage() {}
+	virtual ~MyTabPage(){}
+
+
+	virtual void paint( GraphicsContext* context, Rect* paintRect ) {
+		DefaultTabPage::paint( context, paintRect );
+		
+		Image* img = GraphicsToolkit::createImage( "res:btn.png" );
+		img->setIsTransparent(true);
+
+
+		context->drawImage( paintRect->getTopRight().offset(-1.0 * (2+img->getWidth()),2), img );
+
+		delete img;
+
+	}
+		
+
+};
+
 
 class TabsApplication : public Application {
 public:
 
 	TabsApplication( int argc, char** argv ) : Application(argc, argv) {
+		addCallback( new ClassProcedure1<MouseEvent*,TabsApplication>(this, &TabsApplication::tabPageClicked), "TabsApplication::tabPageClicked" );
+	}
 
+
+	void tabPageClicked( MouseEvent* e ) {
+
+		TabbedPages* pages = (TabbedPages*) getMainWindow()->findComponent( "tabs" );
+
+		TabPage* page = pages->tabPageHitTest( *e->getPoint() );
+		if ( NULL != page && page->getIndex() == 4 ) {
+			Dialog::showMessage( "MyTabPage instance Clicked!" );
+		}
 	}
 
 	virtual bool initRunningApplication(){
@@ -36,6 +71,11 @@ public:
 		setMainWindow(mainWindow);
 		mainWindow->show();
 
+
+
+		TabbedPages* pages = (TabbedPages*) mainWindow->findComponent( "tabs" );
+
+		pages->setTabPage( 4, new MyTabPage() );
 
 		
 		return result;
