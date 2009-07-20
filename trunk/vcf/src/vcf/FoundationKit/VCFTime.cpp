c2f511849b0c358b487f9f59a811ffc24b892da7
smartmobili <smartmobili@cccfda39-7811-0410-ae5f-bc4b32ab4073>
2009-07-20 20:19:35 +0000

    add definition for wince platforms and implement posix time api by using 64bits

--- trunk/vcf/src/vcf/FoundationKit/VCFTime.cpp
+++ trunk/vcf/src/vcf/FoundationKit/VCFTime.cpp
@@ -3,16 +3,19 @@
 #include "vcf/FoundationKit/FoundationKit.h"
 #include "vcf/FoundationKit/VCFTime.h"
 
+#ifdef VCF_WIN32CE
+#undef	time
+#define time		_time64
+struct tm _tmbuf;
+#endif
+
 namespace VCF {
 
 time_t vcftime( time_t *timer )
 {
 	time_t result = 0;
-#ifndef VCF_WIN32CE
 	result = ::time( timer );
-#else
-				
-#endif
+
 	return result;
 }
 
@@ -21,9 +24,8 @@ struct tm* vcflocaltime( const time_t *timer )
 #ifndef VCF_WIN32CE
 	return ::localtime( timer );
 #else
-	static struct tm result;
-
-	return &result;
+	 int err = _localtime64_s(&_tmbuf, timer);
+	 return &_tmbuf;
 #endif
 }
 
