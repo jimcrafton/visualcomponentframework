c2894d2b6456a0e6fb6f386d7a1dcf2dfa205e28
ddiego <ddiego@cccfda39-7811-0410-ae5f-bc4b32ab4073>
2011-04-27 14:51:20 +0000

    added missing files.

---
+++ trunk/vcf/src/vcf/FoundationKit/UnitTransformer.cpp
@@ -0,0 +1,34 @@
+//UnitTransformer.cpp
+
+/*
+Copyright 2000-3004 The VCF Project.
+Please see License.txt in the top level directory
+where you installed the VCF.
+*/
+
+
+#include "vcf/FoundationKit/FoundationKit.h"
+#include "vcf/FoundationKit/UnitTransformer.h"
+
+using namespace VCF;
+
+UnitTransformer* UnitTransformer::currentTransformer_ = NULL;
+
+UnitTransformer* UnitTransformer::getCurrentTransformer()
+{
+	return UnitTransformer::currentTransformer_;
+}
+
+void UnitTransformer::setCurrentTransformer(UnitTransformer* val) 
+{
+	UnitTransformer::currentTransformer_ = val;
+}
+
+String UnitTransformer::xfrmString( const String& value )
+{
+	if ( NULL != UnitTransformer::currentTransformer_ ) {
+		return UnitTransformer::currentTransformer_->transform(value);
+	}
+
+	return value;
+}
---
+++ trunk/vcf/src/vcf/FoundationKit/UnitTransformer.h
@@ -0,0 +1,39 @@
+#ifndef _VCF_UNITTRANSFORMER_H__
+#define _VCF_UNITTRANSFORMER_H__
+//UnitTransformer.h
+
+/*
+Copyright 2000-2004 The VCF Project.
+Please see License.txt in the top level directory
+where you installed the VCF.
+*/
+
+
+#if _MSC_VER > 1000
+#   pragma once
+#endif
+
+
+namespace VCF {
+
+
+	class FOUNDATIONKIT_API UnitTransformer {
+	public:
+		UnitTransformer(){}
+		virtual ~UnitTransformer(){}
+
+		virtual String transform( const String& originalValue ) = 0;
+
+		static UnitTransformer* getCurrentTransformer();
+		static void setCurrentTransformer(UnitTransformer* val);
+
+		static String xfrmString( const String& value );
+	private:
+		static UnitTransformer* currentTransformer_;
+	};
+
+
+};
+
+
+#endif //_VCF_UNITTRANSFORMER_H__
